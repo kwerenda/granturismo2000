d62be6442908931569b94095057b12f77602cf2e
p4r4noj4 <p4r4noj4@gmail.com>
2015-01-18 15:53:56 +0100

    map.js file contains the definition for map, start, and and route

--- web/visualization.js
+++ web/visualization.js
@@ -1,22 +1,9 @@
 
 
-map  = [
-    [0, 0, 0, 0, 0, 1],
-    [0, 1, 1, 0, 0, 1 ],
-    [1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 0, 1,1],
-    [1, 1, 0, 0, 0,1]
-];
 
-start = {x: 3, y:4 };
-end = {x: 0, y:1 };
 
 route1 = [{x:3, y:4}, {x:3, y:1}, {x:2, y:0}, {x:1, y:0}, {x:0, y:1}];
-route = [{x:3,y:3},
-{x:3,y:2},
-{x:2,y:0},
-{x:1,y:0}
-]; //512
+
 route3 = [
 {x:0,y:0},
 {x:2,y:1},
@@ -35,15 +22,12 @@ var mapTileSize = 128;
 var game, gameWidth, gameHeight;
 
 function preload() {
-    //load image which will be used as ground texture
     game.load.image('road', 'assets/road.png');
     game.load.image('dirt', 'assets/dirt.png');
 
-    //  This sets a limit on the up-scale
     game.scale.maxWidth = gameWidth;
     game.scale.maxHeight = gameHeight;
 
-    //  Then we tell Phaser that we want it to scale up to whatever the browser can handle, but to do it proportionally
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.refresh();
 }
@@ -63,29 +47,21 @@ function prepareStaticMap() {
 
 function drawRoute() {
     if(route.length>0) {
-//        var shape = game.add.graphics(0, 0);
         game.context.strokeStyle = 'rgb(30,90,30)';
         game.context.lineWidth = 8;
         game.context.beginPath();
         game.context.moveTo(start.x*scaledMapGrid + scaledMapGrid/2, start.y*scaledMapGrid + scaledMapGrid/2);
-//        shape.moveTo(route[0].x*scaledMapGrid + scaledMapGrid/2, route[0].y*scaledMapGrid + scaledMapGrid/2);
-//        shape.lineStyle(2, 0x0000FF, 1); // width, color (0x0000FF), alpha (0 -> 1) // required settings
-//        shape.beginFill(0xFFFF0B, 1); // color (0xFFFF0B), alpha (0 -> 1) // required settings
         for (var vert = 1; vert < route.length; vert++) {
             game.context.lineTo(route[vert].x*scaledMapGrid + scaledMapGrid/2, route[vert].y*scaledMapGrid + scaledMapGrid/2)
         }
         game.context.lineTo(end.x*scaledMapGrid + scaledMapGrid/2, end.y*scaledMapGrid + scaledMapGrid/2);
-//        shape.endFill();
         game.context.stroke();
         game.context.closePath();
-
-//            new Phaser.Line(route[vert].x, route[vert].y, route[vert + 1].x, route[vert + 1].y);
     }
 }
 
 function create() {
     prepareStaticMap();
-//    drawRoute();
 }
 
 function render() {
