bc99292c4d8049fc653386d36c8569b46ef4258b
p4r4noj4 <p4r4noj4@gmail.com>
2015-01-18 17:01:22 +0100

    auto-creation of map.js file (you need to refresh browser page now in order to get new visualisation)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from src.model import Model, Point
 from src.random_search import RandomSearch
 from src.simulated_annealing import SimulatedAnnealing
+import json
 
 
 if __name__ == '__main__':
@@ -12,20 +13,25 @@ if __name__ == '__main__':
         [1, 1, 0.1, 0.1, 1],
         [1, 1, 0.1, 0.1, 0.1]
     ]
-    n_turns = 4
+    n_turns = 6
     start = Point(3, 4)  # x, y
     finish = Point(0, 1)
     weight_segement = 1.0
-    weight_turn = 5.0
+    weight_turn = 10.0
 
     iterations = 1000
     model = Model(terrain_map, n_turns, start, finish, weight_segement, weight_turn)
     # engine = RandomSearch(model)
     engine = SimulatedAnnealing(model)
     solution = engine.solve(iterations)
-    print("Fitness: {0}".format(solution[0]))
-    for point in solution[1]:
-        print("{{x:{0},y:{1}}}".format(point.x, point.y))
+    print("Fitness: {0}".format([solution[0]]))
+    json_solution = json.dumps([{"x": p.x, "y": p.y} for p in solution[1]])
+    json_map = json.dumps(terrain_map)
+    with open("../web/map.js", "w+") as output_data:
+        output_data.write("map = {0};\n".format(json_map))
+        output_data.write("start = {{x:{0},y:{1}}};\n".format(start.x, start.y))
+        output_data.write("end = {{x:{0},y:{1}}};\n".format(finish.x, finish.y))
+        output_data.write("route = {0};\n".format(json_solution))
 
     # solution - lista punktow i jakosc rozwiazania
 
